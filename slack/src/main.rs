5896678b21b2fefcec889b4f708cee11f7105dd4
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2020-08-27 13:57:24 +0200

    Move out start server and clean up unused fn

--- slack/src/main.rs
+++ slack/src/main.rs
@@ -120,28 +120,22 @@ fn slack_request(req: Form<SlackRequest>) -> Json<SlackResponse> {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![slack_request]).launch();
+    start_server().launch();
 }
 
-// new
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
-}
-
-fn rocket() -> rocket::Rocket {
+fn start_server() -> rocket::Rocket {
     rocket::ignite().mount("/", routes![slack_request])
 }
 
 #[cfg(test)]
 mod test {
-    use super::rocket;
+    use super::start_server;
     use rocket::http::{ContentType, Status};
     use rocket::local::Client;
 
     #[test]
     fn test_slack_request() {
-        let client = Client::new(rocket()).unwrap();
+        let client = Client::new(start_server()).unwrap();
         let mut response = client
             .post("/")
             .body("token=token&team_id=team_id&team_domain=team_domain&enterprise_id=enterprise_id&enterprise_name=enterprise_name&channel_id=channel_id&channel_name=channel_name&user_id=user_id&user_name=user_name&command=command&text=add+vacation+today&response_url=response_url&trigger_id=trigger_id&api_app_id=api_app_id")
@@ -152,6 +146,22 @@ mod test {
         assert_eq!(
             response.body_string(),
             Some("{\"status\":\"vacation is a valid reason.\"}".into())
+        );
+    }
+
+    #[test]
+    fn test_slack_request_invalid_reason() {
+        let client = Client::new(start_server()).unwrap();
+        let mut response = client
+            .post("/")
+            .body("token=token&team_id=team_id&team_domain=team_domain&enterprise_id=enterprise_id&enterprise_name=enterprise_name&channel_id=channel_id&channel_name=channel_name&user_id=user_id&user_name=user_name&command=command&text=add+invalid+reason&response_url=response_url&trigger_id=trigger_id&api_app_id=api_app_id")
+            .header(ContentType::Form)
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::JSON));
+        assert_eq!(
+            response.body_string(),
+            Some("{\"status\":\"invalid is an invalid reason.\"}".into())
         );
     }
 }
